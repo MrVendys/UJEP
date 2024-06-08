167e368b2dcee10906778506dae632227bfe2530
MrVendys <72186908+MrVendys@users.noreply.github.com>
2024-06-08 17:09:08 +0200

    ZKR Repo

---
+++ 2.year/2.semestr/ZKR/ZKR_code/ZKR_code/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ZKR_code // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Code c = new Code();
+            c.Do()
+        }
+    }
+}
