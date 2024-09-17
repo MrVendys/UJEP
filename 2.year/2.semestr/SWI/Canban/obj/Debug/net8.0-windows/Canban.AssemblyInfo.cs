04e7d105bf53236d14d693f55b0b7bb40675ee4a
MrVendys <72186908+MrVendys@users.noreply.github.com>
2024-09-17 17:47:43 +0200

    Cleaning BoardWindow XML

--- 2.year/2.semestr/SWI/Canban/obj/Debug/net8.0-windows/Canban.AssemblyInfo.cs
+++ 2.year/2.semestr/SWI/Canban/obj/Debug/net8.0-windows/Canban.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Canban")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff69c15a150d105fe58ade3a0f9c9a5024884cf1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b954785280287160cc1c2139e8d7657c12d16d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Canban")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Canban")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
